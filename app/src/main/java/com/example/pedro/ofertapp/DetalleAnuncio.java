d6e5d027aad8768965134c2b6a60545292ce2df3
Pedro Valdes <pedrovaldes@trestristestigres.com>
2017-05-07 20:04:05 +0200

    Web service eliminar anuncio

--- app/src/main/java/com/example/pedro/ofertapp/DetalleAnuncio.java
+++ app/src/main/java/com/example/pedro/ofertapp/DetalleAnuncio.java
@@ -2,7 +2,10 @@ package com.example.pedro.ofertapp;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -11,13 +14,28 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Pedro on 06/05/2017.
  */
 public class DetalleAnuncio extends Activity {
+
+    public static final int CONNECTION_TIMEOUT=10000;
+    public static final int READ_TIMEOUT=15000;
 
     private User usuario_loggeado;
     private String idAnuncio, tituloAnuncio, sectorAnuncio, precioAnuncio, provinciaAnuncio, descripcionAnuncio;
@@ -40,8 +58,6 @@ public class DetalleAnuncio extends Activity {
 
         Bundle bundle = getIntent().getExtras();
         setAnuncio(bundle);
-
-        this.usuario_loggeado = (User)getIntent().getExtras().getSerializable("user");
 
         textDetalle = (TextView)findViewById(R.id.tituloDetalle);
         textDetalle.setText(R.string.tituloDetalleAnuncio);
@@ -69,6 +85,7 @@ public class DetalleAnuncio extends Activity {
 
     private void setAnuncio(Bundle bundle) {
 
+        this.usuario_loggeado = (User)bundle.getSerializable("userLogged");
         this.idAnuncio = bundle.getString("idAnuncio");
         this.tituloAnuncio = bundle.getString("tituloAnuncio");
         this.sectorAnuncio = bundle.getString("sectorAnuncio");
@@ -123,5 +140,113 @@ public class DetalleAnuncio extends Activity {
 
     private void deleteAnuncio(String idAnuncio) {
 
+        new AsyncLogin().execute(Integer.parseInt(idAnuncio));
+    }
+
+    private class AsyncLogin extends AsyncTask<Integer, String, String> {
+
+        ProgressDialog pdLoading = new ProgressDialog(DetalleAnuncio.this);
+        HttpURLConnection conn;
+        URL url = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pdLoading.setMessage("Eliminando...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+        }
+
+        @Override
+        protected String doInBackground(Integer... params) {
+
+            try {
+                url = new URL("http://10.0.2.2:80/api/v1/anuncio/"+params[0]);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return "exception";
+            }
+
+            try  {
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("DELETE");
+                conn.setRequestProperty( "charset", "utf-8");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+
+            } catch (IOException e1) {
+
+                e1.printStackTrace();
+                return "exception";
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    //Leemos lo que nos devuelve el servidor
+                    InputStream input = conn.getInputStream();
+                    String result = convertStreamToString(input);
+                    input.close();
+
+                    return result.toString();
+
+                } else {
+                    return ("unsuccesful");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "exception";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            super.onPostExecute(result);
+            pdLoading.dismiss();
+
+            Integer code;
+
+            try {
+
+                JSONObject obj = new JSONObject(result);
+                code = obj.getInt("code");
+
+                if(code.equals(200)) {
+
+                    Toast.makeText(DetalleAnuncio.this, R.string.mensaje_ok_delete_anuncio, Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
+                    i.putExtra("user", usuario_loggeado);
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    getApplicationContext().startActivity(i);
+
+                } else {
+                    Toast.makeText(DetalleAnuncio.this, R.string.mensaje_error_delete_anuncio, Toast.LENGTH_LONG).show();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 }
